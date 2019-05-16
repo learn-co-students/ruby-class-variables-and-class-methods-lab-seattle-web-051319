089e8d85825da568961b823b077586f65380ffee
Noah Fairbairn <noahfairbairn@gmail.com>
2019-05-16 16:27:40 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,61 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count = @@count + 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_hash = {}
+        count = 1
+        @@genres.each do |genre|
+            if genre_hash.has_key?(genre) == false
+                genre_hash[genre] = count
+            else
+                genre_hash[genre] = (count+1)
+            end
+        end
+        genre_hash
+    end
+
+    def self.artist_count
+        artist_hash = {}
+        count = 1
+        @@artists.each do |artist|
+            if artist_hash.has_key?(artist) == false
+                artist_hash[artist] = count
+            else
+                artist_hash[artist] = (count+1)
+            end
+        end
+        artist_hash
+    end
+
+
+
+
+end
